786cf611fd301c0aafe015ccded02ca42a0df0a9
soulevil09 <igorgalloseabra@gmail.com>
2022-10-05 23:35:11 -0300

    Correção

--- main/views.py
+++ main/views.py
@@ -17,9 +17,9 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, 'Cadastro realizado com sucesso')
+            messages.success(request, "Cadastro realizado com sucesso!")
             return redirect('home')
-        messages.error(request, 'Falha no Cadastro do usuário.')
+        messages.error(request, "Falha no cadastro do usuário.")
     form = NovoUsuarioForm()
     context = {'form': form}
     return render(request, template_name='main/register.html', context=context)
